6e9771fc803d52b494cf9472a7d69f58945ddfa0
codenakama <rj.abreu@outlook.com>
2017-07-04 00:03:31 +0100

    user details panel

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -8,11 +8,38 @@ const UserDetailsWrapper = styled.div`
   background-color: #14233c;
   color: #fff;
   padding: 2em;
+  justify-content: space-around;
 `;
+
+const Detail = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  color: #fff;
+`;
+
+const DetailValue = styled.span`font-weight: bold;`;
+const DetailDescription = styled.span``;
 
 class UserDetails extends Component {
   render() {
-    return <UserDetailsWrapper>User details</UserDetailsWrapper>;
+    return (
+      <UserDetailsWrapper>
+        <Detail>
+          <DetailValue>100£</DetailValue>
+          <DetailDescription>Balance</DetailDescription>
+        </Detail>
+        <Detail>
+          <DetailValue>21.80£</DetailValue>
+          <DetailDescription>Spent Today</DetailDescription>
+        </Detail>
+        <Detail>
+          <DetailValue>280£</DetailValue>
+          <DetailDescription>Last Top-up</DetailDescription>
+        </Detail>
+      </UserDetailsWrapper>
+    );
   }
 }
 
