27600b1737e8ea0247ec64c97d819e90a3a9f783
Ricardo Abreu <rj.abreu89@gmail.com>
2018-08-26 23:27:13 +0100

    fix get auth code when static exported

--- pages/monzo-auth.js
+++ pages/monzo-auth.js
@@ -1,13 +1,25 @@
+import { withRouter } from 'next/router';
 import React from 'react';
 import { connect } from 'react-redux';
 // import Spinner from 'react-spinkit';
 import { Flex } from 'rebass';
-import { withRouter } from 'next/router';
 import { fetchToken } from '../store/auth/actions';
+
+const getParameterByName = (name, url) => {
+  if (!url) url = window.location.pathname;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
 
 class AuthPage extends React.Component {
   async componentDidMount() {
-    const { code } = this.props.router.query;
+    const url = window.location.href;
+    const code = getParameterByName('code', url);
+
     this.props.dispatch(fetchToken(code));
   }
 
@@ -27,11 +39,7 @@ class AuthPage extends React.Component {
 
     return (
       <Flex mt={6} justify="center">
-        {isFetchingToken && (
-        <div>
-Loading...
-        </div>
-        )}
+        {isFetchingToken && <div>Loading...</div>}
       </Flex>
     );
   }
@@ -42,7 +50,7 @@ AuthPage.propTypes = {};
 const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated,
   isFetchingToken: state.auth.isFetchingToken,
-  error: state.auth.error,
+  error: state.auth.error
 });
 
 export default connect(mapStateToProps)(withRouter(AuthPage));
